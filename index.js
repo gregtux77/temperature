658ae6c4d50d12252487833ac86e60c883095648
gregtux77 <34283203+gregtux77@users.noreply.github.com>
2017-12-05 22:49:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var install = require('./lib/install.js');
 var uninstall = require('./lib/uninstall.js');
 var temperature = require('./lib/temperature.js');
-
+var command = require('./lib/commande.js');
 
 module.exports = function(sails) {
 
